0fbb88dca8af42838403b76c3a0a521cd4f8f9d2
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-27 11:50:49 -0700

    :construction: Scaffolding markup

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,40 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    // Imports
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <!-- markup and custom components -->
 
-  <div class="card">
-    <Counter />
-  </div>
+    <!-- <Navbar /> -->
+    <!-- <Hero /> -->
+    <!-- <p class="blurb"></p> -->
+    <!-- <section class="works">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+    </section> -->
+    <!-- <section class="about">
+        <h2></h2>
+        <p></p>
+        <DownloadButton />
+    </section> -->
+    <!-- <section class="technologies">
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    </section> -->
+    <!-- <footer class="contact">
+        <h2></h2>
+        <p></p>
+        <div class="socials">
+            <SocialLink />
+            <SocialLink />
+            <SocialLink />
+            <SocialLink />
+        </div>
+    </footer> -->
+    <!-- <Overlay /> -->
+    <!-- <MouseEffect /> -->
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    /* style for internal markup */
 </style>
