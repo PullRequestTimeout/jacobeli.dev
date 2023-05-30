82fc5ee43162608a15e69556f27fdc082f4a7a47
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-29 17:01:04 -0700

    :construction: Mostly finished MouseEffect

--- src/components/MouseEffect.svelte
+++ src/components/MouseEffect.svelte
@@ -1,32 +1,61 @@
 <script>
-    // Create variables to store the mouse coordinates
-    let mouseX = 0
-    let mouseY = 0
+    import { onMount } from "svelte"
 
-    // Function to update the mouse coordinates
-    const updateMousePosition = (event) => {
-        mouseX = event.clientX
-        mouseY = event.clientY
+    let blob
+
+    onMount(() => {
+        document.addEventListener("pointermove", handlePointerMove)
+        return () => {
+            document.removeEventListener("pointermove", handlePointerMove)
+        }
+    })
+
+    function handlePointerMove(event) {
+        const { clientX, clientY } = event
+        blob.animate({ left: `${clientX}px`, top: `${clientY}px` }, { duration: 10000, fill: "forwards" })
     }
-
-    // Event listener to track mouse movement
-    document.addEventListener("mousemove", updateMousePosition)
 </script>
 
-<div on:mousemove={updateMousePosition} style="top: {mouseY}px; left: {mouseX}px;" class="mouse-effect" />
-
-<!-- <div on:mousemove={updateMousePosition} class="mouse-effect" /> -->
+<div on:mousemove={handlePointerMove} bind:this={blob} class="mouse-effect" />
+<div class="blur" />
 
 <style>
     .mouse-effect {
+        top: 50%;
+        left: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+        background-color: rgba(161, 121, 69, 0.5);
+        border-radius: 50%;
+        animation: blob 30s infinite;
+        z-index: -1;
+    }
+
+    .blur {
         top: 0;
         left: 0;
-        position: absolute;
-        height: 35rem;
-        aspect-ratio: 1;
-        transform: translate(-50%, -50%);
-        background-color: #a17945;
-        border-radius: 50%;
-        filter: blur(10rem);
+        height: 100vh;
+        width: 100vw;
+        position: fixed;
+        z-index: 1;
+        backdrop-filter: blur(10rem);
+    }
+
+    @keyframes blob {
+        0% {
+            aspect-ratio: 1;
+            height: 25rem;
+            transform: translate(-50%, -50%) scale(1);
+        }
+        50% {
+            aspect-ratio: 1.5;
+            height: 15rem;
+            transform: translate(-50%, -50%) scale(1.1) rotate(180deg);
+        }
+        100% {
+            aspect-ratio: 1;
+            height: 25rem;
+            transform: translate(-50%, -50%) scale(1) rotate(180deg);
+        }
     }
 </style>
