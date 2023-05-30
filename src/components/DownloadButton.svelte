9d511ead4a6febb79de5d604286800e4aa3d73d7
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-29 22:27:34 -0700

    :construction: Started building CTA button

---
+++ src/components/DownloadButton.svelte
@@ -0,0 +1,51 @@
+<script>
+</script>
+
+<button class="download">Download Resume</button>
+
+<style>
+    .download {
+        position: relative;
+        padding: 1rem 4rem 1rem 2rem;
+        font-size: 1.2rem;
+        border-radius: 2rem;
+        border: none;
+        background-color: #000;
+        transition: 0.2s ease;
+    }
+
+    .download:hover {
+        cursor: pointer;
+        background-color: #a17945;
+    }
+
+    .download:hover::before {
+        transform: translateY(-50%) rotate(30deg);
+        color: #fff;
+    }
+
+    .download::before {
+        content: "→";
+        font-size: 1.8rem;
+        font-family: sans-serif;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        height: 2.5rem;
+        aspect-ratio: 1;
+        border-radius: 50%;
+        background-color: #a17945;
+        color: #000;
+        right: 0.55rem;
+        top: 50%;
+        transform: translateY(-50%);
+        transition: 0.2s ease;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .download {
+            background-color: #fff;
+            color: #000;
+        }
+    }
+</style>
