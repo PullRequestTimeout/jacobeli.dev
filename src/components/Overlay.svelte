833721e9d0691efaaee1b420a3b7bc18dcaa1df3
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-29 17:43:42 -0700

    :lipstick: Built dynamic static overlay component

---
+++ src/components/Overlay.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { onMount, onDestroy } from "svelte"
+
+    let seed = Math.floor(Math.random() * 999) // Initial seed value
+
+    const updateSeed = () => {
+        seed = Math.floor(Math.random() * 999)
+    }
+
+    onMount(() => {
+        const interval = setInterval(updateSeed, 10)
+
+        onDestroy(() => {
+            clearInterval(interval)
+        })
+    })
+</script>
+
+<div class="static-overlay" />
+<svg>
+    <defs>
+        <filter id="static">
+            <feTurbulence type="fractalNoise" baseFrequency="0.75" numOctaves="1" {seed} />
+        </filter>
+    </defs>
+</svg>
+
+<style>
+    .static-overlay {
+        position: fixed;
+        z-index: 1000;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        opacity: 0.1;
+        filter: url(#static);
+    }
+</style>
