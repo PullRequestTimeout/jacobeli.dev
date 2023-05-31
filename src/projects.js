19d7d936dc9d015d56f20456fe637071e57a4637
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-05-30 22:49:14 -0700

    :construction: Added projects object for rudimentary CMS

---
+++ src/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        title: "DirtSurfer",
+        subtitle: "React / Node.js App",
+        date: "2023",
+        description:
+            "Dirt Surfer is a forecasting webapp for moutainbike trail conditions initially focusing on Rossland BC and surrounding area, and then expanding to other regions over time. Frontend built using React with Material UI, backend API built using Express and MongoDB. ",
+        image: "src/assets/dirtsurfer.jpg",
+        video: "/1023AerialsHeroVideo.mp4",
+        link: "https://dirtsurfer.app",
+    },
+    {
+        title: "Rooted In Motion",
+        subtitle: "Website - Healthcare",
+        date: "2023",
+        description:
+            "A landing page website for a local manual therapy/osteopathy business located in Rossland, BC. Work includes photograpy/editing, development and deployment. Web Design in collaboration with Sean Makura.",
+        image: "src/assets/dirtsurfer.jpg",
+        video: "/1023AerialsHeroVideo.mp4",
+        link: "https://rootedinmotion.ca",
+    },
+]
+
+// {
+//     title: "",
+//     subtitle: "",
+//     date: "",
+//     description: "",
+//     image: "",
+//     video: "",
+//     link: "",
+// }
