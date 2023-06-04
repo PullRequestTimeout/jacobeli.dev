3fe4f3d60b51ac97663adb72b00b82e14776532d
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-06-03 18:19:14 -0700

    :construction: Started building component

---
+++ src/components/TechIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+    let theme
+    let lightSVG
+    let darkSVG
+    let illuminated
+</script>
+
+{#if theme == "dark"}
+    <div class="svg-container">
+        {lightSVG}
+        <div>{illuminated}</div>
+    </div>
+{:else if theme == "light"}
+    <div class="svg-container">
+        {darkSVG}
+        <div class="illuminated">{illuminated}</div>
+    </div>
+{/if}
+
+<style>
+</style>
