17c4d9026206426506488c8c47bc7ed4f114428e
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-06-05 16:50:13 -0700

    :construction: Built hero section

---
+++ src/components/Hero.svelte
@@ -0,0 +1,102 @@
+<section>
+    <div class="title">
+        <h3 class="baloo">Jacob Druery</h3>
+        <h2 class="mulish">Creative Developer</h2>
+    </div>
+    <p class="subtitle baloo">Building quality-focused experiences on the web</p>
+</section>
+
+<style>
+    section {
+        margin-top: 30vh;
+        display: grid;
+        gap: 2rem;
+        grid-template-columns: fit-content(8ch);
+    }
+
+    .title {
+        display: flex;
+        flex-direction: column;
+        gap: 0.25rem;
+    }
+    h2,
+    h3 {
+        text-transform: uppercase;
+        font-weight: 300;
+    }
+
+    h2 {
+        font-size: 3rem;
+        /* transform: translateX(-4px); */
+        animation: slide-in 0.5s ease-in-out;
+    }
+
+    /* @keyframes slide-in {
+        from {
+            transform: translateY(100%);
+        }
+        to {
+            transform: translateY(0%);
+        }
+    } */
+
+    @media screen and (min-width: 360px) {
+        h2 {
+            font-size: 3.7rem;
+        }
+    }
+
+    @media screen and (min-width: 640px) {
+        h2 {
+            font-size: 4.5rem;
+        }
+    }
+
+    @media screen and (min-width: 768px) {
+        section {
+            grid-template-columns: fit-content(8ch) auto;
+            margin-top: 40vh;
+            gap: 1rem;
+        }
+
+        h2 {
+            font-size: 5rem;
+        }
+
+        .subtitle {
+            align-self: end;
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+        section {
+            margin-top: 50vh;
+        }
+
+        h2 {
+            font-size: 7rem;
+        }
+    }
+
+    @media screen and (min-width: 1280px) {
+        section {
+            margin-top: 40vh;
+        }
+
+        h2 {
+            font-size: 9rem;
+        }
+    }
+
+    @media screen and (min-width: 1536px) {
+        section {
+            margin-top: 35vh;
+        }
+
+        h2 {
+            font-size: 11rem;
+        }
+    }
+
+    /* 320, 640, 768, 1024, 1280, 1536, 1920 */
+</style>
