66fb8006f80615d15e2a7df277756dfbe5d504df
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-06-05 17:45:44 -0700

    :lipstick: Improved animation and added text prop

--- src/components/DownloadButton.svelte
+++ src/components/DownloadButton.svelte
@@ -1,4 +1,6 @@
 <script>
+    export let text
+
     // Place resume PDF in public folder
     function downloadPDF() {
         const url = "/Jacob-Druery-Resume.pdf"
@@ -14,56 +16,77 @@
     }
 </script>
 
-<button on:click={downloadPDF} class="download">Download Resume</button>
+<button on:click={downloadPDF} class="download"><span>{text}</span></button>
 
 <style>
     .download {
         position: relative;
+        z-index: 300;
         padding: 1rem 4rem 1rem 1rem;
         font-size: 1.2rem;
         border-radius: 2rem;
         border: none;
-        background-color: #000;
+        background-color: var(--clr-black);
         transition: 0.2s ease;
+        overflow: hidden;
+    }
+
+    .download span {
+        position: relative;
+        z-index: 304;
     }
 
     .download:hover {
         cursor: pointer;
-        background-color: #a17945;
-        color: #fff;
+        background-color: var(--clr-gold);
+        color: var(--clr-white);
     }
 
     .download:hover::before {
         transform: translateY(-50%) rotate(30deg);
-        color: #fff;
+        color: var(--clr-white);
+    }
+
+    .download:hover::after {
+        transform: translateY(-50%) scale(10);
     }
 
     .download::before {
         content: "→";
         font-size: 1.8rem;
         font-family: sans-serif;
-        position: absolute;
+        z-index: 303;
         display: flex;
         justify-content: center;
-        height: 2.5rem;
-        aspect-ratio: 1;
-        border-radius: 50%;
-        background-color: #a17945;
-        color: #000;
-        right: 0.55rem;
-        top: 50%;
-        transform: translateY(-50%);
+        background-color: transparent;
+    }
+
+    .download::after {
+        content: "";
+        background-color: var(--clr-gold);
+        z-index: 302;
+    }
+
+    .download::after,
+    .download::before {
         transition: 0.2s ease;
+        position: absolute;
+        transform: translateY(-50%);
+        right: 0.55rem;
+        height: 2.5rem;
+        top: 50%;
+        border-radius: 50%;
+        aspect-ratio: 1;
     }
 
     @media (prefers-color-scheme: dark) {
         .download {
-            background-color: #fff;
-            color: #000;
+            background-color: var(--clr-white);
+            color: var(--clr-black);
         }
 
         .download::before {
-            color: #fff;
+            color: var(--clr-white);
         }
     }
 </style>
