ec85b1647198af3e088479b118042e9f792671b3
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-06-07 22:05:04 -0700

    :boom: Oops default keyword

--- src/data/tech.js
+++ src/data/tech.js
@@ -1,72 +1,72 @@
 export const tech = [
     {
         name: "nodejs",
-        default: "../assets/tech/nodejs.svg",
+        light: "../assets/tech/nodejs.svg",
         color: "../assets/tech/nodejs-color.svg",
     },
     {
         name: "figma",
-        default: "../assets/tech/figma.svg",
+        light: "../assets/tech/figma.svg",
         color: "../assets/tech/figma-color.svg",
     },
     {
         name: "react",
-        default: "../assets/tech/react.svg",
+        light: "../assets/tech/react.svg",
         color: "../assets/tech/react-color.svg",
     },
     {
         name: "github",
-        default: "../assets/tech/github.svg",
+        light: "../assets/tech/github.svg",
         color: "../assets/tech/github-color.svg",
     },
     {
         name: "html",
-        default: "../assets/tech/html.svg",
+        light: "../assets/tech/html.svg",
         color: "../assets/tech/html-color.svg",
     },
     {
         name: "js",
-        default: "../assets/tech/js.svg",
+        light: "../assets/tech/js.svg",
         color: "../assets/tech/js-color.svg",
     },
     {
         name: "svelte",
-        default: "../assets/tech/svelte.svg",
+        light: "../assets/tech/svelte.svg",
         color: "../assets/tech/svelte-color.svg",
     },
     {
         name: "davinci",
-        default: "../assets/tech/davinci.svg",
+        light: "../assets/tech/davinci.svg",
         color: "../assets/tech/davinci-color.svg",
     },
     {
         name: "css",
-        default: "../assets/tech/css.svg",
+        light: "../assets/tech/css.svg",
         color: "../assets/tech/css-color.svg",
     },
     {
         name: "git",
-        default: "../assets/tech/git.svg",
+        light: "../assets/tech/git.svg",
         color: "../assets/tech/git-color.svg",
     },
     {
         name: "adobe",
-        default: "../assets/tech/adobe.svg",
+        light: "../assets/tech/adobe.svg",
         color: "../assets/tech/adobe-color.svg",
     },
     {
         name: "mongodb",
-        default: "../assets/tech/mongodb.svg",
+        light: "../assets/tech/mongodb.svg",
         color: "../assets/tech/mongodb-color.svg",
     },
     {
         name: "analytics",
-        default: "../assets/tech/analytics.svg",
+        light: "../assets/tech/analytics.svg",
         color: "../assets/tech/analytics-color.svg",
     },
     {
         name: "express",
-        default: "../assets/tech/express.svg",
+        light: "../assets/tech/express.svg",
         color: "../assets/tech/express.svg",
     },
 ]
