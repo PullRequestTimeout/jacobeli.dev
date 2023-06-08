fd2fab892167c1535e3d2f1c1140782e3b668038
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-06-07 20:23:01 -0700

    :bento: Added device mockups

--- src/projects.js
+++ src/projects.js
@@ -1,21 +1,21 @@
 export const projects = [
-    {
-        title: "DirtSurfer",
-        subtitle: "React / Node.js App",
-        date: "2023",
-        description:
-            "Dirt Surfer is a forecasting webapp for moutainbike trail conditions initially focusing on Rossland BC and surrounding area, and then expanding to other regions over time. Frontend built using React with Material UI, backend API built using Express and MongoDB. ",
-        image: "src/assets/dirtsurfer.jpg", //Dummy data
-        video: "src/assets/gooeystartpagevideo.mp4", //Dummy data
-        link: "https://dirtsurfer.app",
-    },
+    // {
+    //     title: "DirtSurfer",
+    //     subtitle: "React / Node.js App",
+    //     date: "2023",
+    //     description:
+    //         "Dirt Surfer is a forecasting webapp for moutainbike trail conditions initially focusing on Rossland BC and surrounding area, and then expanding to other regions over time. Frontend built using React with Material UI, backend API built using Express and MongoDB. ",
+    //     image: "src/assets/dirtsurfer.jpg", //Dummy data
+    //     video: "src/assets/gooeystartpagevideo.mp4", //Dummy data
+    //     link: "https://dirtsurfer.app",
+    // },
     {
         title: "Rooted In Motion",
         subtitle: "Website - Healthcare",
         date: "2023",
         description:
             "A landing page website for a local manual therapy/osteopathy business located in Rossland, BC. Work includes photograpy/editing, development and deployment. Web Design in collaboration with Sean Makura.",
-        image: "src/assets/rootedinmotionimg.jpg",
+        image: "src/assets/rootedinmotion.jpg",
         video: "src/assets/rootedinmotionvideo.mp4",
         link: "https://rootedinmotion.ca",
     },
@@ -25,7 +25,7 @@ export const projects = [
         date: "2022",
         description:
             "Gooey (GUI) is a startpage to replace the browser's default new tab in the form of a Firefox-Addon. Using vanilla JS, HTML, and CSS as a project to practice fundamentals, API calls, and build a CRUD GUI from scratch. Drawing from /r/startpages for some inspiration.",
-        image: "src/assets/gooeystartpageimg.jpg",
+        image: "src/assets/gooeystartpage.jpg",
         video: "src/assets/gooeystartpagevideo.mp4",
         link: "https://addons.mozilla.org/en-CA/firefox/addon/gooey-startpage/", //Needs replacing when Chrome version is published
     },
