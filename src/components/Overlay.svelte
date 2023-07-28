cbbccc0caac43b7759c816f71e507a24aad837c8
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-07-28 16:11:04 -0700

    :zap: Overlay is svg if < 1280px, webgl if above. WIP.

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -8,32 +8,26 @@
     let positionBuffer
     let resolutionUniformLocation
     let timeUniformLocation
-
-    let grain
+    let seed = Math.floor(Math.random() * 999) // Initial seed value
     let width = window.innerWidth
-    if (width < 768) {
-        grain = 10
-    } else {
-        grain = 3
-    }
+    if (width >= 1280) {
+        onMount(() => {
+            // Get the WebGL context
+            gl = canvas.getContext("webgl", { antialias: false })
 
-    onMount(() => {
-        // Get the WebGL context
-        gl = canvas.getContext("webgl", { antialias: false })
-
-        // Create the vertex shader
-        const vertexShaderSource = `
+            // Create the vertex shader
+            const vertexShaderSource = `
       attribute vec2 position;
       void main() {
         gl_Position = vec4(position, 0.0, 1.0);
       }
     `
-        const vertexShader = gl.createShader(gl.VERTEX_SHADER)
-        gl.shaderSource(vertexShader, vertexShaderSource)
-        gl.compileShader(vertexShader)
+            const vertexShader = gl.createShader(gl.VERTEX_SHADER)
+            gl.shaderSource(vertexShader, vertexShaderSource)
+            gl.compileShader(vertexShader)
 
-        // Create the fragment shader
-        const fragmentShaderSource = `
+            // Create the fragment shader
+            const fragmentShaderSource = `
       precision mediump float;
       uniform vec2 iResolution;
       uniform float iTime;
@@ -44,7 +38,7 @@
 
         vec4 color = texture2D(iChannel0, uv);
 
-        float strength = ${grain}.0;  // Adjust the grain size (smaller value for smaller grain)
+        float strength = 3.0;  // Adjust the grain size (smaller value for smaller grain)
 
         float x = (uv.x + 4.0) * (uv.y + 4.0) * (iTime * 5.0);  // Adjust the rate of change (larger value for slower change)
         vec4 grain = vec4(mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01) - 0.005) * strength;
@@ -63,48 +57,83 @@
         gl_FragColor = fragColor;
       }
     `
-        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-        gl.shaderSource(fragmentShader, fragmentShaderSource)
-        gl.compileShader(fragmentShader)
+            const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+            gl.shaderSource(fragmentShader, fragmentShaderSource)
+            gl.compileShader(fragmentShader)
 
-        // Create the shader program
-        shaderProgram = gl.createProgram()
-        gl.attachShader(shaderProgram, vertexShader)
-        gl.attachShader(shaderProgram, fragmentShader)
-        gl.linkProgram(shaderProgram)
-        gl.useProgram(shaderProgram)
+            // Create the shader program
+            shaderProgram = gl.createProgram()
+            gl.attachShader(shaderProgram, vertexShader)
+            gl.attachShader(shaderProgram, fragmentShader)
+            gl.linkProgram(shaderProgram)
+            gl.useProgram(shaderProgram)
 
-        // Create the position attribute
-        positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position")
-        positionBuffer = gl.createBuffer()
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]), gl.STATIC_DRAW)
-        gl.enableVertexAttribArray(positionAttributeLocation)
-        gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+            // Create the position attribute
+            positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position")
+            positionBuffer = gl.createBuffer()
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]), gl.STATIC_DRAW)
+            gl.enableVertexAttribArray(positionAttributeLocation)
+            gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
 
-        // Set the uniform variables
-        resolutionUniformLocation = gl.getUniformLocation(shaderProgram, "iResolution")
-        gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height)
+            // Set the uniform variables
+            resolutionUniformLocation = gl.getUniformLocation(shaderProgram, "iResolution")
+            gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height)
 
-        timeUniformLocation = gl.getUniformLocation(shaderProgram, "iTime")
+            timeUniformLocation = gl.getUniformLocation(shaderProgram, "iTime")
 
-        // Render the scene
-        function render() {
-            gl.uniform1f(timeUniformLocation, performance.now() / 1000)
+            // Render the scene
+            function render() {
+                gl.uniform1f(timeUniformLocation, performance.now() / 1000)
 
-            gl.viewport(0, 0, canvas.width, canvas.height)
-            gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
+                gl.viewport(0, 0, canvas.width, canvas.height)
+                gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
+
+                requestAnimationFrame(render)
+            }
 
             requestAnimationFrame(render)
-        }
-
-        requestAnimationFrame(render)
-    })
+        })
+    } else {
+        onMount(() => {
+            const updateSeed = () => {
+                seed = Math.floor(Math.random() * 999)
+            }
+            setInterval(updateSeed, 100)
+        })
+    }
 </script>
 
 <canvas bind:this={canvas} />
+<div class="static-overlay" style="filter: url(#static);" />
+<svg>
+    <defs>
+        <filter id="static">
+            <feTurbulence type="fractalNoise" baseFrequency="0.5" numOctaves="1" {seed} />
+        </filter>
+    </defs>
+</svg>
 
 <style>
+    div {
+        pointer-events: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        opacity: 0.05;
+    }
+
+    svg {
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+    }
+
     canvas {
         display: none;
         pointer-events: none;
@@ -121,6 +150,10 @@
     @media screen and (min-width: 1280px) {
         canvas {
             display: block;
+        }
+
+        .static-overlay {
+            display: none;
         }
     }
 </style>
