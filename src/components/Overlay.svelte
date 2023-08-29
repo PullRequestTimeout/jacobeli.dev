64debf002bf1c2a14ed15c9b4c553803a0884009
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-08-29 12:43:09 -0700

    :bug: Fixing overlay for mobile

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -8,55 +8,58 @@
     let positionBuffer
     let resolutionUniformLocation
     let timeUniformLocation
-    let seed = Math.floor(Math.random() * 999) // Initial seed value
+    // let seed = Math.floor(Math.random() * 999) // Initial seed value
     let width = window.innerWidth
-    if (width >= 1280) {
-        onMount(() => {
+
+    function webglAnimation() {
+        if (width < 1280) {
+            return
+        } else {
             // Get the WebGL context
             gl = canvas.getContext("webgl", { antialias: false })
 
             // Create the vertex shader
             const vertexShaderSource = `
-      attribute vec2 position;
-      void main() {
-        gl_Position = vec4(position, 0.0, 1.0);
-      }
-    `
+attribute vec2 position;
+void main() {
+gl_Position = vec4(position, 0.0, 1.0);
+}
+`
             const vertexShader = gl.createShader(gl.VERTEX_SHADER)
             gl.shaderSource(vertexShader, vertexShaderSource)
             gl.compileShader(vertexShader)
 
             // Create the fragment shader
             const fragmentShaderSource = `
-      precision mediump float;
-      uniform vec2 iResolution;
-      uniform float iTime;
-      uniform sampler2D iChannel0;
+precision mediump float;
+uniform vec2 iResolution;
+uniform float iTime;
+uniform sampler2D iChannel0;
 
-      void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-        vec2 uv = fragCoord.xy / iResolution.xy;
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+vec2 uv = fragCoord.xy / iResolution.xy;
 
-        vec4 color = texture2D(iChannel0, uv);
+vec4 color = texture2D(iChannel0, uv);
 
-        float strength = 3.0;  // Adjust the grain size (smaller value for smaller grain)
+float strength = 3.0;  // Adjust the grain size (smaller value for smaller grain)
 
-        float x = (uv.x + 4.0) * (uv.y + 4.0) * (iTime * 5.0);  // Adjust the rate of change (larger value for slower change)
-        vec4 grain = vec4(mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01) - 0.005) * strength;
+float x = (uv.x + 4.0) * (uv.y + 4.0) * (iTime * 5.0);  // Adjust the rate of change (larger value for slower change)
+vec4 grain = vec4(mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01) - 0.005) * strength;
 
-        // Adjust the grain value based on the UV coordinates
-        float grainValue = (grain.r + grain.g + grain.b) / 3.0;
-        grainValue *= 1.0 - pow(abs(uv.x - 0.5) * 2.0, 15.0);  // Make grain strength depend on the distance from the center horizontally, tweak last number for strength
-        grainValue *= 1.0 - pow(abs(uv.y - 0.5) * 2.0, 15.0);  // Make grain strength depend on the distance from the center vertically, tweak last number for strength
+// Adjust the grain value based on the UV coordinates
+float grainValue = (grain.r + grain.g + grain.b) / 3.0;
+grainValue *= 1.0 - pow(abs(uv.x - 0.5) * 2.0, 15.0);  // Make grain strength depend on the distance from the center horizontally, tweak last number for strength
+grainValue *= 1.0 - pow(abs(uv.y - 0.5) * 2.0, 15.0);  // Make grain strength depend on the distance from the center vertically, tweak last number for strength
 
-        fragColor = color + vec4(grainValue);
-      }
+fragColor = color + vec4(grainValue);
+}
 
-      void main() {
-        vec4 fragColor;
-        mainImage(fragColor, gl_FragCoord.xy);
-        gl_FragColor = fragColor;
-      }
-    `
+void main() {
+vec4 fragColor;
+mainImage(fragColor, gl_FragCoord.xy);
+gl_FragColor = fragColor;
+}
+`
             const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
             gl.shaderSource(fragmentShader, fragmentShaderSource)
             gl.compileShader(fragmentShader)
@@ -93,15 +96,17 @@
             }
 
             requestAnimationFrame(render)
-        })
-    } else {
-        onMount(() => {
-            const updateSeed = () => {
-                seed = Math.floor(Math.random() * 999)
-            }
-            setInterval(updateSeed, 100)
-        })
+        }
     }
+
+    onMount(() => {
+        webglAnimation()
+        window.addEventListener("resize", webglAnimation)
+
+        return () => {
+            window.removeEventListener("resize", webglAnimation)
+        }
+    })
 </script>
 
 <canvas bind:this={canvas} />
@@ -109,7 +114,9 @@
 <svg>
     <defs>
         <filter id="static">
-            <feTurbulence type="fractalNoise" baseFrequency="0.5" numOctaves="1" {seed} />
+            <feTurbulence type="fractalNoise" baseFrequency="0.5">
+                <animate attributeName="seed" dur="1s" values="100;200" repeatCount="indefinite" />
+            </feTurbulence>
         </filter>
     </defs>
 </svg>
@@ -122,7 +129,9 @@
         left: 0;
         width: 100%;
         height: 100vh;
-        opacity: 0.115;
+        opacity: 1;
+        mix-blend-mode: overlay;
+        z-index: 1000;
     }
 
     svg {
